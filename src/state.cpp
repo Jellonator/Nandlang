6f2f241ac3e7e7c75daa73f2ffd0488e127c2078
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2018-04-09 13:24:17 +0300

    Implement AND logic gate

--- src/state.cpp
+++ src/state.cpp
@@ -58,13 +58,21 @@ void fn_iogood(State& state) {
     state.push(bool(std::cin));
 }
 
+/// Implements AND logic gate
+void fn_and(State& state) {
+    bool l = state.pop();
+    bool r = state.pop();
+    state.push(l && r);
+}
+
 const std::map<std::string, FunctionExternal> stdlib = {
     {"putb",   {fn_putb,   1, 0}},
     {"puti8",  {fn_puti8,  8, 0}},
     {"endl",   {fn_endl,   0, 0}},
     {"putc",   {fn_putc,   8, 0}},
     {"getc",   {fn_getc,   0, 8}},
-    {"iogood", {fn_iogood, 0, 1}}
+    {"iogood", {fn_iogood, 0, 1}},
+    {"and",    {fn_and,    2, 1}}
 };
 
 State::State()
